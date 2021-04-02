8ee4e35cd7e318156afc26c73b6abbad1ac4cf2f
Jonathan Porta <jonathan@jonathanporta.com>
2021-04-02 01:58:48 -0600

    Correct template variable typos

--- {{ cookiecutter.project_slug }}/backend.tf
+++ {{ cookiecutter.project_slug }}/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "{{ cookiecutter.tf_backend.s3_bucket }}"
-    key    = "{{ cookiecutter.tf_backend.s3_key }}"
-    region = "{{ cookiecutter.tf_backend.s3_region }}"
+    bucket = "{{ cookiecutter.tf_backend_s3_bucket }}"
+    key    = "{{ cookiecutter.tf_backend_s3_key }}"
+    region = "{{ cookiecutter.tf_backend_s3_region }}"
   }
 }
